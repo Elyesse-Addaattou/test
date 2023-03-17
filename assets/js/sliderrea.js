ad31ac7aeacfe3ecb3e04a842f6811e9a5cf6975
YacineAddaattou <yacine.addaattou@hotmail.com>
2023-03-17 19:16:59 +0100

    slider bug

--- assets/js/sliderrea.js
+++ assets/js/sliderrea.js
@@ -1,7 +1,7 @@
 const slider = document.getElementById("slider");
+const grille = document.querySelector(".grille");
 
 let slideIndex = 1;
-showSlides(slideIndex);
 
 function plusSlides(n) {
     showSlides(slideIndex += n);
@@ -33,21 +33,44 @@ function showSlides(n) {
     
     // Ajouter "active" sur le point cliqué
     dots[slideIndex - 1].classList.add('active');
-  };
 
-  document.body.addEventListener("click",function (e) {
-    console.log(e.target.parentElement);
-    if ( slider.contains(e.target) ) {
-        // document.body.style.backgroundColor= "#333"
-
-
-       
-    }
-    else {
-        slider.style.display = "none"
-        
-        
-    }
-
-  });
+    handleSlides();
+  }
   
+  function handleSlides(){
+
+   document.body.addEventListener("click",function (e){ 
+     console.log(e.target.parentElement);
+     if ( slider.contains(e.target) ) {
+     }
+     else if(grille.style.display="none") {
+         slider.remove(); 
+         grille.style.display="grid";
+   }
+
+  })
+}
+
+  const gridItems = document.querySelectorAll(".grid-item");
+  gridItems.forEach(gridItem => {
+    gridItem.addEventListener("click", handleClickGrid )
+  });
+
+  function handleClickGrid(e){
+    const targetName = e.target.parentNode.dataset.target;
+    const slidesContainer = document.querySelector(".slides--container");
+    for(i=1; i<5 ; i++){
+      const slides = document.createElement("div");
+      slides.classList.add("slides");
+      const image = document.createElement("img");
+      image.src="assets/img/photoSlider/"+targetName+"/"+i+".jpg";
+      image.style="width:100%";
+      slides.appendChild(image);
+      console.log(slides);
+      console.log(slidesContainer);
+      slidesContainer.prepend(slides);
+    }
+    grille.style.display = "none";
+    showSlides(slideIndex);
+  }
+    
